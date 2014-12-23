580d78d20135e0f9925f48cff5fbd56e9de12ac6
confi <confi@outlook.com>
2014-12-23 17:16:44 +0800

    Firs Commit
    1. Create user UI
    2. Create display file name and nextfile button fucntion.

---
+++ InputData.Designer.vb
@@ -0,0 +1,352 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InputData
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InputData))
+        Me.DocInfo = New System.Windows.Forms.Label
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.CheckBox9 = New System.Windows.Forms.CheckBox
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
+        Me.AddKeyWords = New System.Windows.Forms.Button
+        Me.Append = New System.Windows.Forms.Button
+        Me.MoveFile = New System.Windows.Forms.Button
+        Me.Status = New System.Windows.Forms.Label
+        Me.NextFile = New System.Windows.Forms.Button
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.DeleteFolder = New System.Windows.Forms.Button
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DocInfo
+        '
+        Me.DocInfo.AutoSize = True
+        Me.DocInfo.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DocInfo.ForeColor = System.Drawing.SystemColors.InactiveCaption
+        Me.DocInfo.Location = New System.Drawing.Point(6, 17)
+        Me.DocInfo.MaximumSize = New System.Drawing.Size(300, 36)
+        Me.DocInfo.Name = "DocInfo"
+        Me.DocInfo.Size = New System.Drawing.Size(15, 14)
+        Me.DocInfo.TabIndex = 0
+        Me.DocInfo.Text = "--"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox4)
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 153)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(73, 130)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "单元类型"
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(8, 107)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox4.TabIndex = 0
+        Me.CheckBox4.Text = "自控"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(8, 78)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox3.TabIndex = 0
+        Me.CheckBox3.Text = "阀门"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(8, 49)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox2.TabIndex = 0
+        Me.CheckBox2.Text = "仪表"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(8, 20)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "设备"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox9)
+        Me.GroupBox2.Controls.Add(Me.CheckBox5)
+        Me.GroupBox2.Controls.Add(Me.CheckBox6)
+        Me.GroupBox2.Controls.Add(Me.CheckBox7)
+        Me.GroupBox2.Controls.Add(Me.CheckBox8)
+        Me.GroupBox2.Location = New System.Drawing.Point(100, 153)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(87, 130)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "文件类型"
+        '
+        'CheckBox9
+        '
+        Me.CheckBox9.AutoSize = True
+        Me.CheckBox9.Location = New System.Drawing.Point(9, 108)
+        Me.CheckBox9.Name = "CheckBox9"
+        Me.CheckBox9.Size = New System.Drawing.Size(72, 16)
+        Me.CheckBox9.TabIndex = 0
+        Me.CheckBox9.Text = "过时文件"
+        Me.CheckBox9.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(10, 86)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox5.TabIndex = 0
+        Me.CheckBox5.Text = "价格"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Location = New System.Drawing.Point(10, 64)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox6.TabIndex = 0
+        Me.CheckBox6.Text = "图纸"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.AutoSize = True
+        Me.CheckBox7.Location = New System.Drawing.Point(10, 42)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(60, 16)
+        Me.CheckBox7.TabIndex = 0
+        Me.CheckBox7.Text = "说明书"
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.AutoSize = True
+        Me.CheckBox8.Location = New System.Drawing.Point(10, 20)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(48, 16)
+        Me.CheckBox8.TabIndex = 0
+        Me.CheckBox8.Text = "样本"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(200, 153)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "单元品牌"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(200, 174)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(101, 20)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(202, 203)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "关键词"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Enabled = False
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(200, 224)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(101, 20)
+        Me.ComboBox2.TabIndex = 4
+        '
+        'AddKeyWords
+        '
+        Me.AddKeyWords.Enabled = False
+        Me.AddKeyWords.Location = New System.Drawing.Point(202, 253)
+        Me.AddKeyWords.Name = "AddKeyWords"
+        Me.AddKeyWords.Size = New System.Drawing.Size(99, 23)
+        Me.AddKeyWords.TabIndex = 5
+        Me.AddKeyWords.Text = "增加关键词"
+        Me.AddKeyWords.UseVisualStyleBackColor = True
+        '
+        'Append
+        '
+        Me.Append.Location = New System.Drawing.Point(14, 337)
+        Me.Append.Name = "Append"
+        Me.Append.Size = New System.Drawing.Size(85, 23)
+        Me.Append.TabIndex = 6
+        Me.Append.Text = "加入数据库"
+        Me.Append.UseVisualStyleBackColor = True
+        '
+        'MoveFile
+        '
+        Me.MoveFile.Location = New System.Drawing.Point(115, 337)
+        Me.MoveFile.Name = "MoveFile"
+        Me.MoveFile.Size = New System.Drawing.Size(85, 23)
+        Me.MoveFile.TabIndex = 6
+        Me.MoveFile.Text = "移动文件"
+        Me.MoveFile.UseVisualStyleBackColor = True
+        '
+        'Status
+        '
+        Me.Status.AutoSize = True
+        Me.Status.Location = New System.Drawing.Point(16, 398)
+        Me.Status.Name = "Status"
+        Me.Status.Size = New System.Drawing.Size(29, 12)
+        Me.Status.TabIndex = 7
+        Me.Status.Text = "就绪"
+        '
+        'NextFile
+        '
+        Me.NextFile.Location = New System.Drawing.Point(216, 365)
+        Me.NextFile.Name = "NextFile"
+        Me.NextFile.Size = New System.Drawing.Size(85, 23)
+        Me.NextFile.TabIndex = 6
+        Me.NextFile.Text = ">>下一文件"
+        Me.NextFile.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(14, 365)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "打开文件"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DeleteFolder
+        '
+        Me.DeleteFolder.Location = New System.Drawing.Point(217, 337)
+        Me.DeleteFolder.Name = "DeleteFolder"
+        Me.DeleteFolder.Size = New System.Drawing.Size(85, 23)
+        Me.DeleteFolder.TabIndex = 6
+        Me.DeleteFolder.Text = "删除文件夹"
+        Me.DeleteFolder.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.DocInfo)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(289, 135)
+        Me.GroupBox3.TabIndex = 8
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "文件信息"
+        '
+        'InputData
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(314, 427)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Status)
+        Me.Controls.Add(Me.NextFile)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DeleteFolder)
+        Me.Controls.Add(Me.MoveFile)
+        Me.Controls.Add(Me.Append)
+        Me.Controls.Add(Me.AddKeyWords)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "InputData"
+        Me.Text = "Input information"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DocInfo As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox5 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox6 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox7 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox8 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents AddKeyWords As System.Windows.Forms.Button
+    Friend WithEvents Append As System.Windows.Forms.Button
+    Friend WithEvents MoveFile As System.Windows.Forms.Button
+    Friend WithEvents Status As System.Windows.Forms.Label
+    Friend WithEvents NextFile As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DeleteFolder As System.Windows.Forms.Button
+    Friend WithEvents CheckBox9 As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ InputData.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Security.AccessControl
+Imports System.IO
+
+Public Class InputData
+
+    Private nextKeyPressed As Boolean = True
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Visible = False
+        Dim folder As New FolderBrowserDialog
+        folder.Description = "请选择开始整理的文件夹根目录..."
+        folder.ShowNewFolderButton = False
+        folder.ShowDialog()
+        Me.Visible = True
+        Dim fileName As String = folder.SelectedPath
+        DocInfo.ForeColor = Color.Black
+        searchFolder(fileName)
+
+    End Sub
+
+    '选择文件夹，遍历文件夹。
+    Private Sub searchFolder(ByVal folderPath As String)
+
+        '处理当前文件夹下的*.dwg文件
+        Dim s As DirectorySecurity = New DirectorySecurity(folderPath, AccessControlSections.Access)
+
+        If Not (s.AreAccessRulesProtected) Then
+            If Directory.GetFiles(folderPath).Length > 0 Then
+                Dim fileNames As String() = Directory.GetFiles(folderPath)
+                For Each fullName As String In fileNames
+                    Dim sectionNo As Integer = fullName.Split("\").Length - 1
+                    Dim path As String = ""
+                    Dim fileName As String = ""
+                    For i As Integer = 0 To fullName.Split("\").Length - 2
+                        path = path & fullName.Split("\")(i) & "\"
+                    Next
+                    fileName = fullName.Split("\")(sectionNo)
+                    DocInfo.Text = "文件名:   " & fileName & vbCrLf & "文件夹:   " & path & vbCrLf
+                    Do While nextKeyPressed
+                        Application.DoEvents()
+                    Loop
+                    nextKeyPressed = True
+                Next
+            End If
+
+
+            ''递归调用，处理当前文件夹下子文件夹
+            'If Directory.GetDirectories(folderPath).Length > 0 Then
+            '    Dim subDirectories() As String = Directory.GetDirectories(folderPath)
+
+            '    For Each subDirectory As String In subDirectories
+            '        searchFolder(subDirectory)
+            '    Next
+
+            'End If
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub NextFile_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles NextFile.Click
+        nextKeyPressed = False
+    End Sub
+End Class
